36efdeaea80420bd03fff2a5104a0837398478c9
Tiago Scolari <tiago@hashicorp.com>
2023-03-24 10:14:12 +0000

    Changes to packer init

--- lua/packer-init.lua
+++ lua/packer-init.lua
@@ -1,11 +1,18 @@
 local execute = vim.api.nvim_command
 local fn = vim.fn
 
-local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
 
 if fn.empty(fn.glob(install_path)) > 0 then
-  fn.system({'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path})
-  execute 'packadd packer.nvim'
+    fn.system({
+        'git',
+        'clone',
+        '--depth',
+        '1',
+        'https://github.com/wbthomason/packer.nvim',
+        install_path,
+    })
+    execute 'packadd packer.nvim'
 end
 
 local packer_ok, packer = pcall(require, "packer")
@@ -21,7 +28,7 @@ packer.init {
     max_jobs = 50,
     display = {
         open_fn = function()
-            return require("packer.util").float {border = "single"}
+            return require("packer.util").float { border = "single" }
         end
     }
 }
