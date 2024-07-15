b19df063e30bd85139da4da20a5c9ff7913708b4
Tiago Scolari <git@tscolari.me>
2024-07-15 21:22:46 +0100

    Update which-key format

--- lua/keyboard.lua
+++ lua/keyboard.lua
@@ -98,94 +98,93 @@ vim.keymap.set('n', '<C-b>', [[<cmd>lua require('lspsaga.action').smart_scroll_w
 
 vim.keymap.set('n', 'K', [[<cmd>Lspsaga hover_doc<cr>]], { silent = true })
 
-wk.register({
-    D = { vim.lsp.buf.declaration, 'Go to declaration' },
-    d = { telescope.lsp_definitions, 'Go to definition' },
-    y = { vim.lsp.buf.type_definition, 'Go to type' },
-    m = { telescope.lsp_implementations, 'Find implementation' },
-    h = { [[<cmd>Lspsaga lsp_finder<cr>]], 'Smart find references/implementation' },
-    r = { telescope.lsp_references, 'Find references' },
-    K = { [[<cmd>Lspsaga signature_help<cr>]], 'Show signature' },
-    a = { '<Plug>(LiveEasyAlign)', 'EasyAlign', mode = 'x' },
-}, { prefix = 'g', silent = true })
+wk.add({
+    { 'gD', vim.lsp.buf.declaration,             desc = 'Go to declaration' },
+    { 'gd', telescope.lsp_definitions,           desc = 'Go to definition' },
+    { 'gy', vim.lsp.buf.type_definition,         desc = 'Go to type' },
+    { 'gm', telescope.lsp_implementations,       desc = 'Find implementation' },
+    { 'gh', [[<cmd>Lspsaga lsp_finder<cr>]],     desc = 'Smart find references/implementation' },
+    { 'gr', telescope.lsp_references,            desc = 'Find references' },
+    { 'gK', [[<cmd>Lspsaga signature_help<cr>]], desc = 'Show signature' },
+    { 'ga', '<Plug>(LiveEasyAlign)',             desc = 'EasyAlign',                           mode = 'x' },
+})
+
+wk.add({
+    { '<leader> ',  group = "general" },
+    { '<leader> s', ':Alpha<cr>',          desc = 'Home Buffer' },
+    { '<leader> c', telescope.commands,    desc = 'Search commands' },
+    { '<leader> a', telescope.colorscheme, desc = 'Search colorschemes' },
+    { '<leader> h', telescope.help_tags,   desc = 'Help' },
+})
+
+wk.add({
+    { '<leader>f',  group = "files" },
+    { '<leader>ff', telescope.find_files,                                                                      desc = 'File Search' },
+    { '<leader>fo', telescope.buffers,                                                                         desc = 'Buffer Search' },
+    { '<leader>fm', telescope.oldfiles,                                                                        desc = 'Recent Files' },
+    { '<leader>f-', function() require('telescope').extensions.file_browser.file_browser({ cwd = '%:h' }) end, desc = 'File Browser' },
+    { '<leader>f.', '<c-^>',                                                                                   desc = 'Go to last buffer' },
+})
+
+wk.add({
+    { '<leader>g',  group = "git" },
+    { '<leader>gs', ':Git<cr>',             desc = 'git status' },
+    { '<leader>gb', ':Git blame<cr>',       desc = 'git blame' },
+    { '<leader>gc', telescope.git_commits,  desc = 'git commits' },
+    { '<leader>gk', telescope.git_bcommits, desc = 'git commits (buffer)' },
+})
+
+wk.add({
+    { '<leader>h',  group = "hunks" },
+    { '<leader>ht', vgit.toggle_live_guttter,         desc = 'Toggle live gutter' },
+    { '<leader>hs', vgit.buffer_hunk_stage,           desc = 'Stage Hunk' },
+    { '<leader>hp', vgit.buffer_hunk_preview,         desc = 'Preview Hunk' },
+    { '<leader>hu', vgit.buffer_hunk_reset,           desc = 'Undo Hunk' },
+    { '<leader>hR', vgit.buffer_unstage,              desc = 'Reset Buffer' },
+    { '<leader>hl', vgit.buffer_blame_preview,        desc = 'Blame line' },
+    { '<leader>hb', vgit.toggle_live_blame,           desc = 'Toggle live blame' },
+    { '<leader>ha', vgit.toggle_authorship_code_lens, desc = 'Toggle authors' },
+})
+
+wk.add({
+    { '<leader>l',  group = "language-server" },
+    { '<leader>la', ':Lspsaga code_action<cr>',               desc = 'Code Action' },
+    { '<leader>l=', vim.lsp.buf.formatting_sync,              desc = 'Format' },
+    { '<leader>lr', ':Lspsaga rename<cr>',                    desc = 'Rename' },
+    { '<leader>lk', ':Lspsaga hover_doc<cr>',                 desc = 'Doc' },
+    { '<leader>ls', telescope.lsp_dynamic_workspace_symbols,  desc = 'Search Symbols' },
+    { '<leader>ld', "<cmd>Trouble document_diagnostics<cr>",  desc = 'Diagnostics' },
+    { '<leader>lD', "<cmd>Trouble workspace_diagnostics<cr>", desc = 'Workspace Diagnostics' },
+    { '<leader>lt', "<cmd>Vista!!<cr>",                       desc = 'Symbol tree' },
+})
+
+wk.add({
+    { '<leader>b',  group = "buffer" },
+    { '<leader>bd', function() require('bufdelete').bufdelete(0, true) end, desc = 'Delete Buffer' },
+    { '<leader>bl', ':b#<cr>',                                              desc = 'Last buffer' },
+    { '<leader>bn', ':bnext<cr>',                                           desc = 'Next buffer' },
+    { '<leader>bp', ':bprevious<cr>',                                       desc = 'Previous buffer' },
+    { '<leader>bs', telescope.buffers,                                      desc = 'Search buffers' },
+})
+
+wk.add({
+    { '<leader>s',  group = "search" },
+    { '<leader>sg', '<cmd>Grepper<cr>',                  desc = 'Find in directory (quickfix)' },
+    { '<leader>sf', telescope.live_grep,                 desc = 'Find in directory (live)' },
+    { '<leader>sl', ':FZFLines<cr>',                     desc = 'Find in open files' },
+    { '<leader>sb', telescope.current_buffer_fuzzy_find, desc = 'Find in buffer' },
+    { '<leader>sp', require('spectre').open,             desc = 'Search & Replace' },
+})
 
 
-wk.register({
-    name = "+general",
-    s    = { ':Alpha<cr>', 'Home Buffer' },
-    c    = { telescope.commands, 'Search commands' },
-    a    = { telescope.colorscheme, 'Search colorschemes' },
-    h    = { telescope.help_tags, 'Help' },
-}, { prefix = '<leader> ' })
-
-wk.register({
-    name = "+files",
-    f = { telescope.find_files, 'File Search' },
-    o = { telescope.buffers, 'Buffer Search' },
-    m = { telescope.oldfiles, 'Recent Files' },
-    ['-'] = { function() require('telescope').extensions.file_browser.file_browser({ cwd = '%:h' }) end, 'File Browser' },
-    ['.'] = { '<c-^>', 'Go to last buffer' },
-}, { prefix = '<leader>f' })
-
-wk.register({
-    name = "+git",
-    s = { ':Git<cr>', 'git status' },
-    b = { ':Git blame<cr>', 'git blame' },
-    c = { telescope.git_commits, 'git commits' },
-    k = { telescope.git_bcommits, 'git commits (buffer)' },
-}, { prefix = '<leader>g' })
-
-wk.register({
-    name = "+hunks",
-    t    = { vgit.toggle_live_guttter, 'Toggle live gutter' },
-    s    = { vgit.buffer_hunk_stage, 'Stage Hunk' },
-    p    = { vgit.buffer_hunk_preview, 'Preview Hunk' },
-    u    = { vgit.buffer_hunk_reset, 'Undo Hunk' },
-    R    = { vgit.buffer_unstage, 'Reset Buffer' },
-    l    = { vgit.buffer_blame_preview, 'Blame line' },
-    b    = { vgit.toggle_live_blame, 'Toggle live blame' },
-    a    = { vgit.toggle_authorship_code_lens, 'Toggle authors' },
-}, { prefix = '<leader>h' })
-
-wk.register({
-    name  = "+language-server",
-    a     = { ':Lspsaga code_action<cr>', 'Code Action' },
-    ['='] = { vim.lsp.buf.formatting_sync, 'Format' },
-    r     = { ':Lspsaga rename<cr>', 'Rename' },
-    k     = { ':Lspsaga hover_doc<cr>', 'Doc' },
-    s     = { telescope.lsp_dynamic_workspace_symbols, 'Search Symbols' },
-    d     = { "<cmd>Trouble document_diagnostics<cr>", 'Diagnostics' },
-    D     = { "<cmd>Trouble workspace_diagnostics<cr>", 'Workspace Diagnostics' },
-    t     = { "<cmd>Vista!!<cr>", 'Symbol tree' },
-}, { prefix = '<leader>l' })
-
-wk.register({
-    name = "+buffer",
-    d    = { function() require('bufdelete').bufdelete(0, true) end, 'Delete Buffer' },
-    l    = { ':b#<cr>', 'Last buffer' },
-    n    = { ':bnext<cr>', 'Next buffer' },
-    p    = { ':bprevious<cr>', 'Previous buffer' },
-    s    = { telescope.buffers, 'Search buffers' },
-}, { prefix = '<leader>b' })
-
-wk.register({
-    name = "+search",
-    g    = { '<cmd>Grepper<cr>', 'Find in directory (quickfix)' },
-    f    = { telescope.live_grep, 'Find in directory (live)' },
-    l    = { ':FZFLines<cr>', 'Find in open files' },
-    b    = { telescope.current_buffer_fuzzy_find, 'Find in buffer' },
-    p    = { require('spectre').open, 'Search & Replace' },
-}, { prefix = '<leader>s' })
-
-
-wk.register({
-    name  = "+testing",
-    t     = { ':TestNearest<cr>', 'Run Nearest' },
-    f     = { ':TestFile<cr>', 'Run File' },
-    s     = { ':TestSuite<cr>', 'Run Suite' },
-    g     = { ':TestVisit<cr>', 'Goto last ran test' },
-    ['.'] = { ':TestLast<cr>', 'Run Last' },
-}, { prefix = '<leader>t' })
+wk.add({
+    { '<leader>t',  group = "testing" },
+    { '<leader>tt', ':TestNearest<cr>', desc = 'Run Nearest' },
+    { '<leader>tf', ':TestFile<cr>',    desc = 'Run File' },
+    { '<leader>ts', ':TestSuite<cr>',   desc = 'Run Suite' },
+    { '<leader>tg', ':TestVisit<cr>',   desc = 'Goto last ran test' },
+    { '<leader>t.', ':TestLast<cr>',    desc = 'Run Last' },
+})
 
 vim.keymap.set('n', ']t', '<Plug>(ultest-next-fail)')
 vim.keymap.set('n', '[t', '<Plug>(ultest-prev-fail)')
